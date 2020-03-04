3d035cc9a3e10cd46bcb21cbe22ccc09799171ea
Jorge Vizarraga <jorgevizarraga@MacBook-Pro-de-Jorge.local>
2020-03-04 18:42:38 +0100

    new file:   API/foo.cpp 	new file:   cdelele.py

---
+++ cdelele.py
@@ -0,0 +1,9 @@
+from ctypes import cdll
+lib = cdll.LoadLibrary('./API/libfoo.so')
+class Foo(object):
+    def __init__(self):
+        self.obj = lib.Foo_new()
+    def bar(self):
+        lib.Foo_bar(self.obj)
+f = Foo()
+f.bar() #and you will see "Hello" on the screen
