a13713544505e64be5c5aa770e158e47238d68f8
Jorge Vizarraga <jorgevizarraga@MacBook-Pro-de-Jorge.local>
2020-03-22 20:29:48 +0100

    new file:   ../API/init.sh

---
+++ API/init.sh
@@ -0,0 +1,9 @@
+# Para crear el .o meto en terminal:
+
+g++ -c -fPIC foo.cpp -o foo.o
+
+
+# Para crear la librería compartida, meto en el terminal:
+
+g++ -shared -Wl,-install_name,libfctn.so -o libfctn.so  fctn.o
+
