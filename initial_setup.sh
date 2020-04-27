123341d619a58095c85a30fdfe7afe45ec3c92ac
Tu Nombre <jorgevizar96@gmail.com>
2020-04-27 12:33:55 +0200

    modificado:     ../initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,4 +1,7 @@
 
 cd CiA402Device
+python3 setup_PortBase.py  build_ext --inplace
+python3 setup_CiA301CommPort.py  build_ext --inplace
+python3 setup_CiA402SetupData.py  build_ext --inplace
 python3 setup_Cia402device.py  build_ext --inplace
-cd..
+
