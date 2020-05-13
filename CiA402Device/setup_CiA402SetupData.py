c4f1ba9f39adc15f6437e2ab5a7036f11e54d3ba
Tu Nombre <jorgevizar96@gmail.com>
2020-05-13 18:48:56 +0200

    modificado:     ../CiA402Device/__pycache__/CiA301CommPort.cpython-36.pyc 	modificado:     ../CiA402Device/__pycache__/CiA402SetupData.cpython-36.pyc 	modificado:     ../CiA402Device/__pycache__/Cia402device.cpython-36.pyc 	modificado:     ../CiA402Device/__pycache__/PortBase.cpython-36.pyc 	nuevo archivo:  ../CiA402Device/__pycache__/SocketCanPort.cpython-36.pyc 	modificado:     ../CiA402Device/setup_CiA402SetupData.py 	modificado:     ../CiA402Device/setup_PortBase.py

--- CiA402Device/setup_CiA402SetupData.py
+++ CiA402Device/setup_CiA402SetupData.py
@@ -4,7 +4,7 @@ from distutils.core import setup, Extension
 name = "CiA402SetupData"
 version = "1.0"
 
-example_module = Extension('_CiA402SetupData', sources=['CiA402SetupData.cpp', 'CiA402SetupData.i'], swig_opts=['-builtin', '-Isrc', '-c++'])
+example_module = Extension('_CiA402SetupData', sources=['CiA402SetupData.cpp', 'CiA402SetupData.i'], swig_opts=['-Isrc', '-c++'])
 
 setup(name = name, ext_modules = [example_module])
 
--- CiA402Device/setup_PortBase.py
+++ CiA402Device/setup_PortBase.py
@@ -4,6 +4,6 @@ from distutils.core import setup, Extension
 name = "PortBase"
 version = "1.0"
 
-example_module = Extension('_PortBase', sources=['PortBase.cpp','PortBase.i'], swig_opts=['-builtin', '-c++'])
+example_module = Extension('_PortBase', sources=['PortBase.cpp','PortBase.i'], swig_opts=['-Isrc', '-c++'])
 
 setup(name = name, ext_modules = [example_module])
