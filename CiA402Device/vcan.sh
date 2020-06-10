b9fb5a69862696561243ddddfb76e872a016716b
Tu Nombre <jorgevizar96@gmail.com>
2020-06-10 20:22:36 +0200

    modificado:     ../CiA402Device/vcan.sh

--- CiA402Device/vcan.sh
+++ CiA402Device/vcan.sh
@@ -1,7 +1,7 @@
-ip link add dev vcan0 type vcan
-ip link add dev vcan1 type vcan
-ip link set up vcan0
-ip link set up vcan1
+sudo ip link add dev vcan0 type vcan
+sudo ip link add dev vcan1 type vcan
+sudo ip link set up vcan0
+sudo ip link set up vcan1
 
 #Son dos puertos que se comunican
 en una pestaña terminal haces:
