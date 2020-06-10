583bc0193be6ace0441cf7c4c331f59359edc03a
Tu Nombre <jorgevizar96@gmail.com>
2020-06-10 19:56:19 +0200

    modificado:     ../CiA402Device/vcan.sh

--- CiA402Device/vcan.sh
+++ CiA402Device/vcan.sh
@@ -2,3 +2,13 @@ ip link add dev vcan0 type vcan
 ip link add dev vcan1 type vcan
 ip link set up vcan0
 ip link set up vcan1
+
+#Son dos puertos que se comunican
+en una pestaña terminal haces:
+candump vcan1 #esto hara que todos los cansend se muestren aqui
+en la otra mandas mensajes haciendo:
+cansend vcan0 123#0011
+
+#hecho esto se muestra en la terminal de candump:
+vcan1 123 [2] 00 11
+
